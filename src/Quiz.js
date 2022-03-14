78f6dcde48c420ca0ea1f58513404abcc5b3f736
SB22SU <Alexandru.Adeaconitei@ing.com>
2022-03-14 15:03:19 +0200

    chore: module 8

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,7 +15,7 @@ class Quiz extends Component {
     return (
       <div>
         {isQuizEnd ? (
-          <QuizEnd />
+          <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
         ) : (
           <QuizQuestion
             className="QuizQuestion"
@@ -29,6 +29,10 @@ class Quiz extends Component {
 
   showNextQuestion() {
     this.setState({ quiz_position: this.state.quiz_position + 1 });
+  }
+
+  handleResetClick() {
+    this.setState({ quiz_position: 1 });
   }
 }
 
